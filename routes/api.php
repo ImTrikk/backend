4bc39ba17a8b135abe850a866e814e2f3c1ed3db
ImTrikk <130761529+ImTrikk@users.noreply.github.com>
2023-10-19 00:42:01 +0800

    added api controlller

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\Api\CarouselItemsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CarouselController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// api routes
+Route::get('/carousel', [CarouselItemsController::class, 'index']);
